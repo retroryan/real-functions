b37a8383fb75aaa926721758430261572f71e56c
Ryan Knight <ryan@grandcloud.com>
2023-11-01 10:45:44 -0600

    test

--- packages/sample/hello/hello.py
+++ packages/sample/hello/hello.py
@@ -1,6 +1,21 @@
+import os
+
 def main(args):
       name = args.get("name", "stranger")
       greeting = "Hello " + name + "!"
       print(greeting)
-      return {"body": greeting}
+
+      REAL_SPACES_ACCESS = os.getenv('REAL_SPACES_ACCESS')
+      REAL_SPACES_KEY = os.getenv('REAL_SPACES_KEY')
+
+      data = {'message': 'Hello, world!'}
+      response = {
+            'statusCode': 200,
+            'headers': {
+                  'Content-Type': 'application/html'
+            },
+            'body': '<html><h1>Hello, world!</h1></html>'
+      }
+
+      return response
   
